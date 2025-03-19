494df127eb25459e3874d8f0bb549afdd0067f9f
KOTFAN <kotfan.empire@gmail.com>
2025-03-19 15:17:45 +0200

    prepared for work, got all the elements

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+//data
+const MainScores = [0, 0];
+let isGamePlaying = true;
+let currentScore = 0;
+
+//page content
+const diceEl = document.querySelector('.dice')
+const mainScoreP0 = document.getElementById('score--0')
+const mainScoreP1 = document.getElementById('score--1')
+const currentScoreP0 = document.getElementById('current--0')
+const currentScoreP1 = document.getElementById('current--1')
+const rollBtn = document.querySelector('.btn--roll')
+const holdBtn = document.querySelector('.btn--bold')
+const newGameBtn = document.querySelector('.btn--new')
+
+
+//start conditionals
+mainScoreP0.textContent = MainScores[0];
+mainScoreP1.textContent = MainScores[1];
+diceEl.classList.add('hidden')
